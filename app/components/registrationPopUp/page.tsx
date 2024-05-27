642d6ea72491190976eb3abf5299dd5fc40bcb25
Sumit Ambulkar <sumit14ambulkar@gmail.com>
2024-05-27 21:06:24 +0530

    Removed Dynamic values

--- app/components/registrationPopUp/page.tsx
+++ app/components/registrationPopUp/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-function RegistrationPopup({ showPopup, setShowPopup, message }:any) {
+function RegistrationPopup({ showPopup, setShowPopup }:any) {
   return (
     <div className={`fixed inset-0 flex items-center justify-center ${showPopup ? '' : 'hidden'}`} onClick={() => setShowPopup(false)}>
       <div className="fixed inset-0 bg-black opacity-50"></div>
-      <div className={`${message.color} p-6 rounded shadow-lg z-10`}>
-        <p className="text-lg font-semibold">{message.mes}</p>
+      <div className={` p-6 rounded shadow-lg z-10`}>
+        <p className="text-lg font-semibold"></p>
       </div>
     </div>
   );
